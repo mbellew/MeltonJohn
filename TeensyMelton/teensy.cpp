2ddb65bef4164b026e6d29674ee2ea82a868c723
matthewb <matthewb@labkey.com>
2021-05-25 21:02:45 -0700

    msgeq7

--- TeensyMelton/teensy.cpp
+++ TeensyMelton/teensy.cpp
@@ -68,6 +68,7 @@ void LOG_format(int n)
 }
 
 
+#if USE_I2S || USE_ADC
 #if USE_I2S
 AudioInputI2S adc;                        // AudioBoard
 AudioControlSGTL5000 audioShield;
@@ -139,16 +140,128 @@ struct SoundFFT
 
         if (1 == 1)
         {
-            _print("ret   ");
-            _format((int) (ret.bass * 1000.0));
-            _format((int) (ret.mid * 1000.0));
-            _format((int) (ret.treb * 1000.0));
-            _println();
+            LOG_print("ret   ");
+            LOG_format((int) (ret.bass * 1000.0));
+            LOG_format((int) (ret.mid * 1000.0));
+            LOG_format((int) (ret.treb * 1000.0));
+            LOG_println();
         }
         return true;
     }
 };
+#endif
 
+
+#if USE_MSGEQ7
+#include "MD_MSGEQ7.h"
+
+class SoundFFT
+{
+    const int eqVDD = 19;
+    const int eqRESET = 18;
+    const int eqSTROBE = 17;
+    const int eqOUTL = 16;
+    const int eqOUTR = 15;
+    const int eqGND = 14;
+    const unsigned frameInterval = 1000/44;
+
+    MD_MSGEQ7 MSGEQ7;
+
+    unsigned long frame_count = 0;
+    unsigned long nextFrameStartTime = 0;
+
+    float h_levels[MAX_BAND]        = {1000,1000,1000,1000,1000,1000,1000};
+    float h_inv_levels[MAX_BAND]    = {1.0/1000,1.0/1000,1.0/1000,1.0/1000,1.0/1000,1.0/1000,1.0/1000};  // running sum of inv levels
+    float h_inv_minlevels[MAX_BAND] = {1.0/400,1.0/500,1.0/600,1.0/700,1.0/600,1.0/500,1.0/500};    // running sum (< levels[i])
+    float h_vol_level = 1000;
+    float bass_att = 0;
+    float mid_att = 0;
+    float treb_att = 0;
+
+
+public:
+    SoundFFT() : MSGEQ7(eqRESET, eqSTROBE, eqOUTL)
+    {
+    }
+
+    void begin()
+    {
+        analogReadResolution(13);
+        pinMode(eqGND,OUTPUT);
+        digitalWrite(eqGND,0);
+        pinMode(eqVDD,OUTPUT);
+        digitalWrite(eqVDD,1);
+        delay(1);
+        MSGEQ7.begin();
+        nextFrameStartTime = millis() + frameInterval;
+    };
+
+    bool next(Spectrum &spectrum) 
+    {
+        float raw_level[MAX_BAND];
+
+        if (millis() < nextFrameStartTime)
+            return false;
+        nextFrameStartTime += frameInterval;
+
+        MSGEQ7.read();
+
+        for (size_t i=0; i<MAX_BAND; i++)
+        {
+        frame_count++;
+        float level = fmaxf((float)MSGEQ7.get(i), 1.0f);
+        float inv_level = 1.0/level;
+        h_levels[i]     = h_levels[i] * 0.99 + level * 0.01;
+        h_inv_levels[i] = h_inv_levels[i] * 0.99 + inv_level * 0.01;
+        if (inv_level > h_inv_levels[i])
+        {
+            if (inv_level > h_inv_minlevels[i] || frame_count < 100)
+            h_inv_minlevels[i] = h_inv_minlevels[i] * 0.99 + inv_level * 0.01;
+            else
+            h_inv_minlevels[i] = h_inv_minlevels[i] * 0.999 + inv_level * 0.001;
+        }
+        raw_level[i] = level;
+        }
+
+        float avg_level[MAX_BAND];
+        float curr_level[MAX_BAND];
+        float v = 0.0;
+
+        // skip band 7
+        for (size_t i=0; i<6; i++)
+        {
+        float min_level = 1.0/h_inv_minlevels[i];
+        avg_level[i]  = fmaxf(h_levels[i] - min_level, 20);
+        curr_level[i] = fmaxf(raw_level[i] - min_level, 0);
+        v += curr_level[i];
+        }
+        v /= 6.0;
+        if (frame_count < 100)
+        h_vol_level = h_vol_level * 0.99 + v * 0.01;
+        else
+        h_vol_level = h_vol_level * 0.995 + v * 0.005;
+
+        float avg_v = fmaxf(h_vol_level,20);
+        float b     = (curr_level[0] + curr_level[1]) / 2;
+        float avg_b = ( avg_level[0] +  avg_level[1]) / 2;
+        float m     = (curr_level[2] + curr_level[3]) / 2;
+        float avg_m = ( avg_level[2] +  avg_level[3]) / 2;
+        float t     = (curr_level[4] + curr_level[5]) / 2;
+        float avg_t = ( avg_level[4] +  avg_level[5]) / 2;
+
+        const float formula[3] = {1.0, 1.0, 0.0};
+        spectrum.bass = (formula[0]*b) / (formula[1]*avg_v + formula[2]*avg_b);
+        spectrum.mid  = (formula[0]*m) / (formula[1]*avg_v + formula[2]*avg_m);
+        spectrum.treb = (formula[0]*t) / (formula[1]*avg_v + formula[2]*avg_t);
+        spectrum.bass_att = bass_att = (bass_att * 5 + spectrum.bass) / 6;
+        spectrum.mid_att  = mid_att  = ( mid_att * 5 + spectrum.mid) / 6;
+        spectrum.treb_att = treb_att = (treb_att * 5 + spectrum.treb) / 6;
+        spectrum.vol = v / avg_v;
+
+        return  true;
+    }
+};
+#endif // USE_MSGEQ7
 
 /** MAIN **/
 
@@ -157,7 +270,7 @@ struct SoundFFT
 DMAMEM int octo_displayMemory[IMAGE_SIZE * 6];
 int octo_drawingMemory[IMAGE_SIZE * 6];
 OctoWS2811 octo_leds(IMAGE_SIZE, octo_displayMemory, octo_drawingMemory, WS2811_GRB | WS2811_800kHz);
-class RenderOcto : _DMXWriter
+class RenderOcto : OutputLED
 {
     OctoWS2811 octo_leds;
 
@@ -172,7 +285,7 @@ public:
         leds.show();
     }
 
-    void write(CRGB data[], size_t count) override
+    void write(const CRGB data[], unsigned count) override
     {
         size_t count = min(count, IMAGE_SIZE);
         for (size_t i = 0; i < count; i++)
@@ -187,9 +300,11 @@ public:
 #endif
 
 
+
+
 #if OUTPUT_TEENSYDMX
 #include <TeensyDMX.h>
-class RenderTeensyDMX : public _DMXWriter
+class RenderTeensyDMX : public OutputLED
 {
     ::qindesign::teensydmx::Sender dmxTx;
 
@@ -205,7 +320,7 @@ public:
         dmxTx.resume();
     }
 
-    void write(CRGB data[], size_t count) override
+    void write(const CRGB data[], unsigned count) override
     {
         dmxTx.set(0,(uint8_t*)data,count*3);
     }
@@ -250,7 +365,7 @@ public:
 class SoundFFT sound;
 #if OUTPUT_TEENSYDMX
 class RenderTeensyDMX outputTeensyDmx(Serial1);
-class RenderTeensyDMX &output=outputTeensyDmx;
+class OutputLED *output=&outputTeensyDmx;
 #endif
 Renderer *renderPattern = createRenderer();
 
